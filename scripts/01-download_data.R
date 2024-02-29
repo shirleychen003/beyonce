988175addca3741c9b3b442f59d5c5a6d4b4aac7
shirkey <sshirleyy.chen@mail.utoronto.ca>
2024-02-29 09:40:41 -0500

    add NASA APOD

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,27 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from NASA API
+# Author: Rohan Alexander
+# Date: 29 February 2023
+# Contact: rohan.alexander@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: -
+# Any other information needed: -
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(httr)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+NASA <- 
+  GET("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2024-02-28")
 
+content(NASA)
+
+content(NASA)$explanation
+
+knitr::include_graphics(content(NASA)$url)
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
